f7785ab7aca9d91096438847e3a7d17885cf7c8d
NacOJerk <nacojerk@hotmail.com>
2016-07-21 10:43:00 +0300

    Some base stuff

--- src/com/kirelcodes/pokecraft/Pokecraft.java
+++ src/com/kirelcodes/pokecraft/Pokecraft.java
@@ -3,5 +3,12 @@ package com.kirelcodes.pokecraft;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Pokecraft extends JavaPlugin{
-
+	private static Pokecraft instance = null;
+	@Override
+	public void onEnable() {
+		instance = this;
+	}
+	public static Pokecraft getInstance() {
+		return instance;
+	}
 }
