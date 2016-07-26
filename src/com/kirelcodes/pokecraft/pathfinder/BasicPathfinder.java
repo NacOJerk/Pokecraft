3ec5c7aefa81a071796608a58ac6dd3197fb378f
NacOJerk <nacojerk@hotmail.com>
2016-07-26 14:35:34 +0300

    Started work on the pokemon

---
+++ src/com/kirelcodes/pokecraft/pathfinder/BasicPathfinder.java
@@ -0,0 +1,48 @@
+package com.kirelcodes.pokecraft.pathfinder;
+
+public abstract class BasicPathfinder {
+	/**
+	 * When this method returns true the pathFinder would start working
+	 * 
+	 * @return
+	 */
+	public abstract boolean shouldStart();
+
+	/**
+	 * By default this method returns the shouldStart Once it returns false the
+	 * pathFinder goes on the waiting list agian
+	 * 
+	 * @return
+	 */
+	public boolean keepWorking() {
+		return shouldStart();
+	}
+
+	/**
+	 * This method is called once the path finding process starts
+	 */
+	public void onStart() {
+	}
+
+	/**
+	 * Called after each time the update task method is called Could be used to
+	 * create delays for example
+	 */
+	public void afterTask() {
+	}
+
+	/**
+	 * Called every tick Basically the most important task
+	 */
+	public abstract void updateTask();
+
+	/**
+	 * Basically if the path finder should be the last one running and the one
+	 * setting the navagation
+	 * 
+	 * @return
+	 */
+	public boolean canBeInterrupted() {
+		return true;
+	}
+}
--- src/com/kirelcodes/pokecraft/pokemons/Pokemon.java
+++ src/com/kirelcodes/pokecraft/pokemons/Pokemon.java
@@ -1,56 +1,127 @@
 package com.kirelcodes.pokecraft.pokemons;
 
+import java.io.File;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+
+import com.kirelcodes.miniaturepets.api.pets.APIMob;
+import com.kirelcodes.miniaturepets.api.pets.APIMobContainer;
 import com.kirelcodes.pokecraft.pokemons.enums.PokeGender;
 import com.kirelcodes.pokecraft.pokemons.enums.PokeState;
 import com.kirelcodes.pokecraft.pokemons.enums.PokeType;
 
 public class Pokemon {
-	
-	private float currentHealth;
+
 	private PokeType type;
 	private PokeGender gender;
 	private float exp;
 	private int level;
 	private PokeState state;
-	
-	@Deprecated
+	/**
+	 * GAL FILL WAT IS CATCHRATE
+	 */
+	protected static float catchRate = 0;
+	/**
+	 * The pokemon max health
+	 */
+	protected static double maxHealth = 40;
+	/**
+	 * The navigator mob type
+	 */
+	protected static EntityType anchorType = EntityType.CHICKEN;
+	/**
+	 * The navigator mob type name
+	 */
+	protected static String anchorTypeName = "Chicken";
+	/**
+	 * The speed of the pokemon
+	 */
+	protected static double speed = 0.2;
+	/**
+	 * The pokemon model file
+	 */
+	protected static File model = null;
+	/**
+	 * The pokemon name
+	 */
+	protected static String name = "pokemon";
+	private static APIMobContainer mobContainer = null;
+	private APIMob anchorMob = null;
+
+	public Pokemon(Location loc) {
+		if (mobContainer == null) {
+			mobContainer = new APIMobContainer(model, name, maxHealth, speed,
+					anchorType, anchorTypeName);
+		}
+		anchorMob = mobContainer.spawnMob(loc);
+	}
+
 	public static double getMaxHealth() {
-		return 0;
+		return maxHealth;
 	}
 
-	@Deprecated
-	public float getCurrentHealth() {
-		return currentHealth;
+	public double getCurrentHealth() {
+		return getModelMob().getHealth();
 	}
 
-	@Deprecated
 	public PokeType getType() {
 		return type;
 	}
 
-	@Deprecated
 	public PokeGender getGender() {
 		return gender;
 	}
 
-	@Deprecated
 	public float getExp() {
 		return exp;
 	}
 
-	@Deprecated
 	public int getLevel() {
 		return level;
 	}
-	
-	@Deprecated
+
 	public static float getCatchRate() {
-		return 0;
+		return catchRate;
 	}
-	
-	@Deprecated
-	public PokeState getStatus(){
+
+	public PokeState getStatus() {
 		return state;
 	}
+
+	public APIMob getModelMob() {
+		return anchorMob;
+	}
+
+	public Location getLocation() {
+		return getModelMob().getLocation();
+	}
+
+	public String getName() {
+		return getModelMob().getName();
+	}
+
+	public void setHealth(double health) {
+		getModelMob().setHealth(health);
+	}
+	public void teleport(Location loc) {
+		getModelMob().teleport(loc);
+	}
+
+	public void teleport(Entity e) {
+		teleport(e.getLocation());
+	}
+
+	public boolean setTargetLocation(Location loc) throws Exception{
+		return getModelMob().setTargetLocation(loc);
+	}
 	
+	public Location getTargetLocation(){
+		return getModelMob().getTargetLocation();
+	}
+	
+	public boolean onTargetLocation(){
+		return getModelMob().onTargetLocation();
+	}
 }
